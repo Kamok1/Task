6dee19b2a90ed580c38c793a7fa1826fb7ee60b5
Kamok1 <kkamiss12@gmail.com>
2023-04-26 18:38:47 +0200

    change model

---
+++ Models/Catfact.cs
@@ -0,0 +1,7 @@
+﻿namespace Models;
+
+public record Catfact()
+{
+    public string Fact { get; set; } = string.Empty;
+    public int Length { get; set; }
+}
--- Models/Responses/CatfactResponse.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Models.Response;
-
-public record CatfactResponse()
-{
-  public string Fact { get; set; } = string.Empty;
-  public int Length { get; set; } 
-}
